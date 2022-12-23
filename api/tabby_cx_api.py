b5e086dbbc9b38151fd665ca35eaea229acfc77e
sunveil <mikhailov@icc.ru>
2022-12-23 21:05:26 +0800

    Save changes

--- api/tabby_cx_api.py
+++ api/tabby_cx_api.py
@@ -1,22 +1,35 @@
 import uvicorn
-from fastapi import UploadFile, FastAPI
+from fastapi import FastAPI, UploadFile
+from starlette.middleware.cors import CORSMiddleware
 
-from html_parse.html_parser import parse
-from init_api import app, PORT
+from manager import TabbyCxManager
 
+PORT = 3000
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
     if not file:
         return {"message": "No upload file sent"}
     else:
-        tables = parse(file)
+        manager = TabbyCxManager()
+        tables = manager.parse_html(file)
         json_tables = []
         for table in tables:
             json_tables.append(table.to_dict())
         return {"filename": file.filename, "tables": json_tables}
 
-
+    
 def run_api() -> None:
-    uvicorn.run(host='0.0.0.0', port=PORT)
-
+    uvicorn.run("init_api:app", host='0.0.0.0', port=PORT)
